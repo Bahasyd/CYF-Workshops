bce82e07e5baaf7a0458f9960b4be37b8f94656a
Bahasyd <baha.syd@gmail.com>
2025-02-06 01:42:49 +0000

    first

--- js1-wk1-eval/level-200.js
+++ js1-wk1-eval/level-200.js
@@ -3,6 +3,7 @@
 // Use documentation to evaluate Math.trunc() and Math.floor() for each of the numbers below.
 // Write your answers down below:
 
+
 // a) 10.53   For example, Math.trunc(10.53) evaluates to 10, Math.floor(10.53) evaluates to 10
 // b) 170.105
 // c) -0.34
